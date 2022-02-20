1a12ac9cbc6c38d45262f207de9c702a64e4b4cb
Marcos Moraes <mdmoraes.mopsus@gmail.com>
2022-02-20 12:47:17 -0300

    Adicionar arquivos de projeto.

---
+++ MestreDetalhe1/Migrations/202202201507295_FirstMigration.cs
@@ -0,0 +1,72 @@
+﻿namespace MestreDetalhe1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cliente",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Venda",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Data = c.DateTime(nullable: false),
+                        ClienteId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cliente", t => t.ClienteId, cascadeDelete: true)
+                .Index(t => t.ClienteId);
+            
+            CreateTable(
+                "dbo.Item",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Quantidade = c.Double(nullable: false),
+                        ProdutoId = c.Int(nullable: false),
+                        VendaId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Produto", t => t.ProdutoId, cascadeDelete: true)
+                .ForeignKey("dbo.Venda", t => t.VendaId, cascadeDelete: true)
+                .Index(t => t.ProdutoId)
+                .Index(t => t.VendaId);
+            
+            CreateTable(
+                "dbo.Produto",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Estoque = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Item", "VendaId", "dbo.Venda");
+            DropForeignKey("dbo.Item", "ProdutoId", "dbo.Produto");
+            DropForeignKey("dbo.Venda", "ClienteId", "dbo.Cliente");
+            DropIndex("dbo.Item", new[] { "VendaId" });
+            DropIndex("dbo.Item", new[] { "ProdutoId" });
+            DropIndex("dbo.Venda", new[] { "ClienteId" });
+            DropTable("dbo.Produto");
+            DropTable("dbo.Item");
+            DropTable("dbo.Venda");
+            DropTable("dbo.Cliente");
+        }
+    }
+}
---
+++ MestreDetalhe1/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MestreDetalhe1.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public double Quantidade { get; set; }
+
+        // Produto
+        public int ProdutoId { get; set; }
+        public virtual Produto Produto { get; set; }
+
+        // Venda
+        public int VendaId { get; set; }
+        public virtual Venda Venda { get; set; }
+    }
+}
